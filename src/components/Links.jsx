50c01b4cd6554241e0be97c10b6450103768e75f
Rioba-Ian-Felix <riobafelix@gmail.com>
2022-10-27 22:59:53 +0300

    "main section, socials and links"

---
+++ src/components/Links.jsx
@@ -0,0 +1,12 @@
+export default function Links() {
+    return (
+        <div className="links">
+            <a href="#">Twitter Link</a>
+            <a href="#">Zuri Team</a>
+            <a href="#">Zuri Books</a>
+            <a href="#">Python Books</a>
+            <a href="#">Background  Check for Coders</a>
+            <a href="#">Design Books</a>
+        </div>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
-import myprofile from "../assets/PXL_20220319_122847252.MP.jpg"
+import myprofile from "../assets/sqaured_profile.jpg"
+import Links from "./Links"
 
 export default function Main() {
     return (
         <div className="main__content">
-            <h1>Hello Mum.</h1>
             <img src={myprofile} className="profile" alt="Ian Rioba" />
+            <h1>Ian Rioba</h1>
+            <Links />
         </div>
     )
 }
